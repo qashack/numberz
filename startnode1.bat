0c2483f54eda225b610878f9452dcc89215db773
chandru <chandrashekar@qashack.com>
2017-11-13 15:27:20 +0530

    commit on 13/11-1

--- startnode1.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\harsha\Documents
-java  -Dwebdriver.chrome.driver=chromedriver.exe -jar selenium-server-standalone-3.5.3 -role webdriver -hub http://localhost:4444/grid/register -port 5555
-pause
